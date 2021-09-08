6d8d4187c79ecff91e0a8dc3fd4b64f1285b33f0
holjeffcomb <72577303+holjeffcomb@users.noreply.github.com>
2021-09-08 11:55:37 -0700

    install express async handler

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Book = require('../models').Book;
-
-/* Handler function to wrap each route. */
-function asyncHandler(cb){
-  return async(req, res, next) => {
-    try {
-      await cb(req, res, next)
-    } catch(error){
-      // Forward error to the global error handler
-      next(error);
-    }
-  }
-}
+var asyncHandler = require('express-async-handler');
 
 /* GET home page. */
 router.get('/', asyncHandler(async (req, res) => {
