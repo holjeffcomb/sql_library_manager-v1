b3fae307b07955e72502ab7d557d9ca3e97d0f01
holjeffcomb <72577303+holjeffcomb@users.noreply.github.com>
2021-09-16 21:01:01 -0700

    final update

--- routes/index.js
+++ routes/index.js
@@ -2,35 +2,91 @@ var express = require('express');
 var router = express.Router();
 var Book = require('../models').Book;
 var asyncHandler = require('express-async-handler');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
-/* GET home page. */
+// redirect to homepage
 router.get('/', asyncHandler(async (req, res) => {
   res.redirect('/books');
 }));
 
+// main homepage
 router.get('/books', asyncHandler(async(req, res) => {
-  const books = await Book.findAll();
-  res.render('index', { books });
+  const page = parseInt(req.query.page)
+  !page || page <=0 ? res.redirect('?page=1') : null
+  const limit = 10
+  const {count, rows} = await Book.findAndCountAll({
+    order: [['createdAt', 'DESC']],
+    limit,
+    offset: (page -1) *limit
+  })
+  const pageCount = Math.ceil(count / limit)
+  page > pageCount ?
+  res.redirect(`?page=${pageCount}`) : null
+  let links = 1
+  res.render('index', {books: rows, pageCount, links})
 }));
 
+// search handler
+router.get('/books/search', asyncHandler(async(req,res) =>{
+  const search = req.query.search.toLowerCase()
+  const page = parseInt(req.query.page)
+  !page || page <=0 ? res.redirect(`search?search=${search}&page=1`) : null
+  const limit = 10
+  const {count, rows} = await Book.findAndCountAll({
+    where:{
+      [Op.or]:[
+        {
+          title:{[Op.like]: `%${search}%`}
+        },
+        {
+          author:{[Op.like]: `%${search}%`}
+        },
+        {
+          genre:{[Op.like]: `%${search}%`}
+        },
+        {
+          year:{[Op.like]: `%${search}%`}
+        },
+      ]
+    },
+    order: [['createdAt', 'DESC']],
+    limit,
+    offset: (page -1) *limit
+  });
+  if(count > 0){
+    let links = 1
+    const pageCount = Math.ceil(count / limit)
+    page > pageCount ?
+    res.redirect(`?search=${search}&page=${pageCount}`) : null
+    res.render('index', {books: rows, pageCount, links, search})
+  }else{
+    res.render('book-not-found', {search})
+  }
+}))
+
+// new book handler
 router.get('/books/new', (req, res) => {
-  console.log('test');
   res.render('new-book');
 });
 
+// new book post handler
 router.post('/books/new', asyncHandler(async(req, res) => {
   let book;
   try {
     book = await Book.create(req.body);
     res.redirect('/books/' + book.id);
   } catch (error) {
-    if(error.name === 'SequelizeValidationError') {
+    if(error.name === "SequelizeValidationError") { // checking the error
       book = await Book.build(req.body);
-      res.render('/books/new', { book, errors: error.errors, title: "New Book" });
-    }
+      res.render("new-book", { book, errors: error.errors })
+    } else {
+      throw error
+    }  
   }
 }));
 
+// get individual book handler
 router.get('/books/:id', asyncHandler(async(req, res) => {
   const book = await Book.findByPk(req.params.id);
   if (book) {
@@ -40,6 +96,7 @@ router.get('/books/:id', asyncHandler(async(req, res) => {
   }
 }));
 
+// update individual book handler
 router.post('/books/:id', asyncHandler(async(req, res) => {
   let book;
   try {
@@ -61,6 +118,7 @@ router.post('/books/:id', asyncHandler(async(req, res) => {
   }
 }));
 
+// delete book handler
 router.post('/books/:id/delete', asyncHandler(async(req, res) => {
   const book = await Book.findByPk(req.params.id);
   if(book) {
